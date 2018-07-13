94a4327da0732c3dbb5f66054226ab64116c2dfa
Arnab Purohit <arnab.purohit@cern.ch>
2018-07-13 17:18:52 +0200

    Updated DataFormats/Common, QGTagger and WeightsCountProducer

--- RecoJets/JetProducers/python/QGTagger_cfi.py
+++ RecoJets/JetProducers/python/QGTagger_cfi.py
@@ -5,6 +5,7 @@ QGTagger = cms.EDProducer('QGTagger',
   jetsLabel		= cms.string('QGL_AK4PFchs'),
   srcRho 		= cms.InputTag('fixedGridRhoFastjetAll'),		
   srcVertexCollection	= cms.InputTag('offlinePrimaryVerticesWithBS'),
+  vertexIndex           = cms.uint32(0),
   useQualityCuts	= cms.bool(False)
 )
 
